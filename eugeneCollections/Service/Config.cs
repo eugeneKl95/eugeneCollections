119fe0780dbb53c753c87962e8f3c6bfb4078590
Valentyna-K <kvalentyna@hotmail.com>
2022-01-17 18:10:32 +0200

    Добавил 2 версии AppSetting, написал connectionString, создал класс-обертку Config.cs

---
+++ eugeneCollections/Service/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eugeneCollections.Service
+{
+    public class Config
+    {
+        public string ConnectionString { get; set; }
+    }
+}
