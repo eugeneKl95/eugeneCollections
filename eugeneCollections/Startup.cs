e1b1321e93447f53d397629135d4e5f0cc22d533
Valentyna-K <kvalentyna@hotmail.com>
2022-01-17 18:18:58 +0200

    внес изменения в Startup, сопоставил поля класса Config с полями из appsetting

--- eugeneCollections/Startup.cs
+++ eugeneCollections/Startup.cs
@@ -1,7 +1,9 @@
+using eugeneCollections.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -11,11 +13,14 @@ using System.Threading.Tasks;
 
 namespace eugeneCollections
 {
+    
     public class Startup
     {
-       
+       public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config()); //connection config from appsetting.json
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddSessionStateTempDataProvider();
