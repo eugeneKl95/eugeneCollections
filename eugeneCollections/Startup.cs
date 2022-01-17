eadca48479b0103f2d352bb71bdd64e06c192e64
Valentyna-K <kvalentyna@hotmail.com>
2022-01-17 16:24:54 +0200

    загружен макет сайта, подключені пакеті Nuget пакеты и нужные расширения.

--- eugeneCollections/Startup.cs
+++ eugeneCollections/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -15,24 +16,25 @@ namespace eugeneCollections
        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddSessionStateTempDataProvider();
         }
 
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) //information about errors
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
+            app.UseStaticFiles(); //add supporting static files
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints => // registration need routes
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");               
             });
         }
     }
