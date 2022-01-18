366dca19fc437ba6616cdebe6793697abe9efd2d
Valentyna-K <kvalentyna@hotmail.com>
2022-01-18 16:17:09 +0200

    Добавил 2/7 интерфейсов

---
+++ eugeneCollections/Domain/Repositories/Abstract/ICollectionRepository.cs
@@ -0,0 +1,18 @@
+﻿using eugeneCollections.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eugeneCollections.Domain.Repositories.Abstract
+{
+    interface ICollectionRepository
+    {
+        public void AddCollection();
+        public void DeleteCollection(int id);
+        public void UpdateCollection(int id);
+        public void SaveCollection(Collection entity);
+        public IQueryable<Collection> GetCollections();
+        public IQueryable<Collection> GetCollectionsByUser(Guid Id);
+    }
+}
