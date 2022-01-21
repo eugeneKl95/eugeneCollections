a663e01e027d2a81350258938de60e7cbb1dcd6f
Valentyna-K <kvalentyna@hotmail.com>
2022-01-21 15:43:50 +0200

    add page Collections

---
+++ eugeneCollections/Controllers/CollectionsController.cs
@@ -0,0 +1,20 @@
+﻿using eugeneCollections.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eugeneCollections.Controllers
+{
+    public class CollectionsController : Controller
+    {
+        private readonly DataManager dataManager;
+        public CollectionsController(DataManager dataManager) => this.dataManager = dataManager;
+            
+        public IActionResult Index()
+        {
+            return View("Collections",dataManager.Themes.GetThemes());
+        }
+    }
+}
